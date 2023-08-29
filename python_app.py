37071a3f5d26442af4ad9cff8c619e9877c3cb84
LeonNjoroge <leonmuraguri@gmail.com>
2023-08-29 22:41:18 +0300

    First commit

---
+++ python_app.py
@@ -0,0 +1,6 @@
+# Binary Search takes a list of objects and Halfs them until it finds its target
+#function that takes a list and target parameter
+#multiple variables : middle, start, end, steps
+#recursion or while loop
+#increase the steps each time a split is done
+#conditions to track target position
